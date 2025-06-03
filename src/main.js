ccbbe2c5fcda455967f7931e475522fd3ce60687
Tsuneo Hakoyama <tunehako@gmail.com>
2025-06-04 06:45:04 +0900

    [ref#003] Implementation: Add new title to incomplete-list

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
 import './style.css'
 
+const onClickAdd = () => {
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
+
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    div.className = "todo-item";
+    const p = document.createElement("p");
+    p.className = "todo-title";
+    p.innerText = inputText;
+
+    div.appendChild(p);
+    li.appendChild(div);
+    
+    document.getElementById("incomplete-list").appendChild(li);
+};
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
