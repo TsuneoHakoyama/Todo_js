01ba4e3c8e3982bf54914f9502a5d38a85a83a78
Tsuneo Hakoyama <tunehako@gmail.com>
2025-06-04 13:13:12 +0900

    [ref#004] Implementation: Functions to delete titles, move  a title to the complete list and back a title to the incomplete-list

--- src/main.js
+++ src/main.js
@@ -10,8 +10,31 @@ const onClickAdd = () => {
     const p = document.createElement("p");
     p.className = "todo-title";
     p.innerText = inputText;
+    const completeButton = document.createElement("button");
+    completeButton.className = "button";
+    completeButton.innerText = "完了";
+    completeButton.addEventListener("click", () => {
+        const moveTarget = completeButton.closest("li");
+        completeButton.nextElementSibling.remove();
+        completeButton.remove();
+        const backButton = document.createElement("button");
+        backButton.className = "button";
+        backButton.innerText = "戻す";
+        moveTarget.firstElementChild.appendChild(backButton);
+        document.getElementById("complete-list").appendChild(moveTarget);
+    
+    });
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "button";
+    deleteButton.innerText = "削除";
+    deleteButton.addEventListener("click", () => {
+        const deleteTarget = deleteButton.closest("li");
+        document.getElementById("incomplete-list").removeChild(deleteTarget);
+    });
 
     div.appendChild(p);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
     li.appendChild(div);
     
     document.getElementById("incomplete-list").appendChild(li);
